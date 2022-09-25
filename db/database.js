b4492e065b903db99183941260229c5550c6b83f
eabra <emab21@student.bth.se>
2022-09-25 19:07:42 +0200

    change to mongo atlas dsn

--- db/database.js
+++ db/database.js
@@ -5,14 +5,14 @@
 
 const mongo = require("mongodb").MongoClient;
 // const config = require("./config.json");
-// const dbName = "editor";
+const dbName = "editor";
 const collectionName = "docs";
 
 const database = {
     getDb: async function getDb() {
         let dsn = `mongodb+srv://texteditor:${process.env.ATLAS_PASSWORD}@cluster0.cekx6wh.mongodb.net/?retryWrites=true&w=majority`;
         // let dsn = `mongodb://localhost:27017/${dbName}`;
-
+        console.log(process.env.NODE_ENV);
         if (process.env.NODE_ENV === 'test') {
             dsn = "mongodb://localhost:27017/test";
         }
@@ -21,7 +21,7 @@ const database = {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        const db = await client.db();
+        const db = await client.db(dbName);
         const collection = await db.collection(collectionName);
 
         return {
