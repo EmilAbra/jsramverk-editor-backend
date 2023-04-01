63a942cdbe0d823b31d69bc265ba21bd8bbb6cf4
eabra <emab21@student.bth.se>
2023-04-01 18:00:07 +0200

    add new comment type

--- graphql/doc.js
+++ graphql/doc.js
@@ -6,15 +6,18 @@ const {
     GraphQLBoolean,
 } = require('graphql');
 
+const CommentType = require("./comment.js");
+
 const DocType = new GraphQLObjectType({
     name: 'Doc',
     description: 'This represents a Document',
     fields: () => ({
+        _id: { type: new GraphQLNonNull(GraphQLString) },
         name: { type: new GraphQLNonNull(GraphQLString) },
-        content: { type: GraphQLString },
-        allowed_users: { type: new GraphQLList(GraphQLString) },
+        content: { type: new GraphQLNonNull(GraphQLString) },
+        allowed_users: { type: new GraphQLNonNull(GraphQLList(GraphQLString)) },
         codeMode: { type: new GraphQLNonNull(GraphQLBoolean) },
-        comments: { type: new GraphQLList(GraphQLString) }
+        comments: { type: new GraphQLList(CommentType) }
     })
 })
 
